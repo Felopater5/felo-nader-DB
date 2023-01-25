bc8a3bbafab26d52cb901d63c66649daac8cd27a
Felopater Nader Bahe <Felopater Nader Bahe@DESKTOP-P36V07E>
2023-01-26 00:14:08 +0200

    Add project files.

---
+++ felo nader DB/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace felo_nader_DB
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
